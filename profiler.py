71681731377685eb5c832e23704cf935aaa98d9e
Miles Anthony <teafttomsbonvathe@gmx.net>
2023-01-24 00:00:00 -0500

    docs: Generate Sphinx documentation

--- profiler.py
+++ profiler.py
@@ -138,4 +138,4 @@ if __name__ == '__main__':
         print("Skipping GPU workload simulation as NVML is not initialized.")
 
 
-# Commit timestamp: 2024-06-05 00:00:00 - 153
+# Commit timestamp: 2023-01-24 00:00:00 - 419
