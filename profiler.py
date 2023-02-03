881d07dd6a44984b944ae592d72eebb6fa113661
Miles Anthony <teafttomsbonvathe@gmx.net>
2023-02-03 00:00:00 -0500

    test: Expand test coverage for new features

--- profiler.py
+++ profiler.py
@@ -138,4 +138,4 @@ if __name__ == '__main__':
         print("Skipping GPU workload simulation as NVML is not initialized.")
 
 
-# Commit timestamp: 2023-05-24 00:00:00 - 992
+# Commit timestamp: 2023-02-03 00:00:00 - 405
